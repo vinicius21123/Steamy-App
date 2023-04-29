4219e25bea05253fdeb430e8608d772b1de39005
viniciusName <vinicius.rodrigues.uk@outlook.com>
2023-04-29 13:56:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Route, Link, BrowserRouter as Router, Routes} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
